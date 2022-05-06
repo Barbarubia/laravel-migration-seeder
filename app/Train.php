ee04619439fce2394c646f79e2efb86f2de768fa
Barbarubia <dnl.desimone@gmail.com>
2022-05-06 15:35:04 +0200

    modificata logica del faker

--- app/Train.php
+++ app/Train.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Train extends Model
 {
-    //
+    public $timestamps = false;
+    protected $fillable = [
+        'train_company',
+        'train_code',
+        'departure_railway',
+        'destination_railway',
+        'departure_time',
+        'arrival_time',
+        'n_couches',
+        'on_time',
+        'deleted'
+    ];
 }
--- database/seeds/TrainsTableSeeder.php
+++ database/seeds/TrainsTableSeeder.php
@@ -1,8 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
-use Faker\Generator as Faker;
 use App\Train;
+use Faker\Generator as Faker;
 
 class TrainsTableSeeder extends Seeder
 {
@@ -15,16 +15,21 @@ class TrainsTableSeeder extends Seeder
     {
         $faker = \Faker\Factory::create('it_IT');
         for ($_i = 1; $_i <= 200; $_i++) {
-            $newTrain = new Train();
-            $newTrain->train_company = $faker->company();                   // string('train_company', 30);
-            $newTrain->train_code = $faker->regexify('[A-Z]{2}[0-9]{4}');   // string('train_code', 6);
-            $newTrain->departure_railway = $faker->city();                  // string('departure_railway', 50);
-            $newTrain->destination_railway = $faker->city();                // string('destination_railway', 50);
-            $newTrain->departure_time = $faker->time('H:i');                // time('departure_time');
-            $newTrain->arrival_time = $faker->time('H:i');                  // time('arrival_time');
-            $newTrain->n_couches = $faker->randomDigitNotNull();            // tinyInteger('n_couches')->nullable();
-            $newTrain->on_time = $faker->boolean();                         // boolean('on_time');
-            $newTrain->deleted = $faker->boolean();                         // boolean('deleted');
+            $trainData = [
+                'train_company'         => $faker->company(),
+                'train_code'            => $faker->regexify('[A-Z]{2}[0-9]{4}'),
+                'departure_railway'     => $faker->city(),
+                'destination_railway'   => $faker->city(),
+                'departure_time'        => $faker->time('H:i'),
+                'arrival_time'          => $faker->time('H:i'),
+                'n_couches'             => $faker->randomDigitNotNull(),
+                'on_time'               => $faker->boolean(),
+                'deleted'               => $faker->boolean()
+            ];
+
+            $train = new Train();
+            $train->fill($trainData);
+            $train->save();
         }
     }
 }
