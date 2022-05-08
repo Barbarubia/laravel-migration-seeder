d63623be899ffd7ead3b18311fb1233b94c6a51f
Barbarubia <dnl.desimone@gmail.com>
2022-05-08 18:13:29 +0200

    modifica generatore dati fake

--- database/seeds/TrainsTableSeeder.php
+++ database/seeds/TrainsTableSeeder.php
@@ -13,16 +13,28 @@ class TrainsTableSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
+        $arrTrainsCompanies = [
+            'FlixTrain',
+            'Ryantrain',
+            'ItaTreni',
+            'Trenoo',
+            'EasyTrain',
+            'Ciuf Ciuf Trains',
+            'Train 2 Go',
+            'EuroTrain',
+            'Boolean 56'
+        ];
+
         $faker = \Faker\Factory::create('it_IT');
         for ($_i = 1; $_i <= 200; $_i++) {
             $trainData = [
-                'train_company'         => $faker->word(),
+                'train_company'         => $arrTrainsCompanies[$faker->numberBetween(0, 8)],
                 'train_code'            => $faker->regexify('[A-Z]{2}[0-9]{4}'),
                 'departure_railway'     => $faker->city(),
                 'destination_railway'   => $faker->city(),
                 'departure_time'        => $faker->time('H:i'),
                 'arrival_time'          => $faker->time('H:i'),
-                'n_couches'             => $faker->randomDigitNotNull(),
+                'n_couches'             => $faker->numberBetween(3, 7),
                 'on_time'               => $faker->boolean(),
                 'deleted'               => $faker->boolean()
             ];
